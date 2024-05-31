9223c3cad2b65d6b74797dde706d2ad00226c339
GET MOUSSED <moussa.fof.pro@gmail.com>
2024-05-31 12:13:26 +0200

    upgrade to lastest version of dependencies xD

--- src/App.js
+++ src/App.js
@@ -1,31 +1,21 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Header";
 import Home from "./Home";
 import Checkout from "./Checkout";
 
 const App = () => {
-  return (
-    <Router>
-      <div>
-        <Switch>
-          <Route path="/checkout">
-            <Header />
-            <Checkout />
-          </Route>
-          <Route path="/login">
-            <Header />
-            <h1>Login</h1>
-          </Route>
-          <Route path="/">
-            <Header />
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+	return (
+		<Router>
+			<Header />
+			<Routes>
+				<Route path="/checkout" element={<Checkout />} />
+				<Route path="/login" element={<h1>Login</h1>} />
+				<Route path="/" element={<Home />} />
+			</Routes>
+		</Router>
+	);
 };
 
 export default App;
